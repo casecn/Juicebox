f580679ef923a901415af83875ec3419c009e8ab
Craig Case <case2cn@gmail.com>
2023-07-04 21:35:31 -0600

    wrote addTagsToPosts & getAllTags.  Added Select to crate tags to return added tags.

--- db/tags.js
+++ db/tags.js
@@ -1,13 +1,15 @@
 const { client } = require("./index");
+const { getPostById } = require("./posts");
 
 async function createTableTags() {
   try {
     console.log("3.1  Creating 'tags' table");
     await client.query(
-        `CREATE TABLE tags (
+      `CREATE TABLE tags (
         id SERIAL PRIMARY KEY,
         name varchar(255) UNIQUE NOT NULL);
-      `);
+      `
+    );
     console.log("#### - Finished creating 'tags' table - ####");
   } catch (error) {
     console.error("Error creating 'tags' tables!");
@@ -31,7 +33,8 @@ async function createTablePost_Tags() {
   }
 }
 /**** Functions *******/
-async function createTag( tagData ) {
+async function createTags(tagData) {
+  let insertResults = [];
   let i = 1;
   const placeHolderString = Object.keys(tagData)
     .map(() => {
@@ -40,25 +43,69 @@ async function createTag( tagData ) {
       return placeHolder;
     })
     .join(", ");
-    let sql = `INSERT INTO tags(name)
+  let sql = `INSERT INTO tags(name)
     VALUES ${placeHolderString} 
     ON CONFLICT (name) DO NOTHING RETURNING *`;
+
+  let selectSQL = `Select * from tags WHERE name IN (${placeHolderString})`;
+
   try {
     const { rows } = await client.query(sql, tagData);
-    if (!rows.length) 
-    {
-        return null;
+    if (!rows.length) {
+      return null;
     }
-    return rows;
+    try {
+      const { rows } = await client.query(selectSQL, tagData);
+      if (!rows) {
+        return null;
+      }
+      insertResults = rows;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+    return insertResults;
   } catch (error) {
     console.error(error);
     throw error;
   }
 }
 
+async function getAllTags() {
+  const { rows } = await client.query(`SELECT * FROM tags;`);
+  return rows;
+}
+
+async function createPostTag(postId, tagId) {
+  
+    const sql = `INSERT INTO post_tags ("postId", "tagId")
+    VALUES ($1, $2) 
+    ON CONFLICT ("postId", "tagId") DO NOTHING`;
+  try {
+    await client.query(sql, [postId, tagId]);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+async function addTagsToPosts(postId, tagList) {
+    try {
+        const createPostTagPromises = tagList.map(
+            tag => createPostTag(postId, tag.id)
+        );
+        await Promise.all(createPostTagPromises);
+
+        return getPostbyID(postId);
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
 /**** Export Functions *******/
 module.exports = {
   createTableTags,
   createTablePost_Tags,
-  createTag,
+  createTags,
+  getAllTags,
 };
