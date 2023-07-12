90c482bea60914e2ae2cd27ef72cd2596c86d21f
Craig Case <case2cn@gmail.com>
2023-07-12 00:18:05 -0600

    added simple api/login route

--- api/users.js
+++ api/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const usersRouter = express.Router();//define router
-const { getAllUsers } = require("../db");
+const { getAllUsers, getUserByUsername} = require("../db");
 
 usersRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
@@ -11,6 +11,33 @@ usersRouter.use((req, res, next) => {
 usersRouter.get('/', async (req, res) =>{
   const users = await getAllUsers();
   res.send({ users });
+});
+
+usersRouter.post('/login', async(req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError", 
+      message: "Please supply both a username and password" 
+    });
+  }
+  try {
+    const user = await getUserByUsername(username);
+
+    if (user && user.password == password) {
+      res.send({ message: "you're logged in!" });
+    } else {
+      next({
+        name: 'IncorrectCredentialsError',
+        message: 'Username or password is incorrect'
+      });
+    }
+  } catch(error) {
+    console.log(error);
+    throw error;
+  }
+
 });
 
 module.exports = usersRouter;
